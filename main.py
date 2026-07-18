cf83daf8f30d462dab1bfe5de9841045aa280ccd
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-07-18 10:24:31 +0530

    feat: Helper function to validate github URL

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from config.settings import settings
-from repository.clone_repo import get_repository_name
+from repository.clone_repo import get_repository_name, validate_github_url
 
 print("=" * 50)
 print(settings.app_name)
@@ -9,3 +9,7 @@ print(f"Repo    : {settings.repository_path}")
 print("=" * 50)
 
 print(get_repository_name("https://github.com/microsoft/vscode"))
+
+print(validate_github_url("hello"))
+print(validate_github_url("https://google.com"))
+print(validate_github_url("https://github.com/langchain-ai/langchain"))
--- repository/clone_repo.py
+++ repository/clone_repo.py
@@ -18,3 +18,12 @@ def get_repository_name(url: str) -> str:
   repo_name = Path(parsed_url.path).stem
   return repo_name
 
+
+def validate_github_url(url: str) -> bool:
+  parsed_url = urlparse(url=url)
+  
+  return (
+    parsed_url.scheme in ("http", "https") 
+    and parsed_url.netloc == "github.com"
+    and len(parsed_url.path.strip("/").split("/")) >= 2
+  )
